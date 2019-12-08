4bf55abe50fd6a7b23354d0c5d97422bc70d7a66
Poisson <fc5ntb@splm.siemens.com>
2019-12-08 20:20:02 +0100

    add map metric and min of loss

--- scripts/tf_log_analyser.py
+++ scripts/tf_log_analyser.py
@@ -4,13 +4,25 @@ import math
 
 
 f = open('log.txt')
-pattern = "INFO:tensorflow:loss = "
+loss_pattern = "INFO:tensorflow:loss = "
+map_pattern = "DetectionBoxes_Precision/mAP = "
 loss = []
+map_error = []
+
 for line in f:
-    if re.search(pattern, line):
-        loss.append(math.log(float(re.split(r'[=,]',line)[1])))
-        print(re.split(r'[=,]',line)[1])
-        
-    
+    if re.search(loss_pattern, line):
+        loss.append(float(re.split(r'[=,]',line)[1]))
+        #print(re.split(r'[=,]',line)[1])
+    if re.search(map_pattern, line):
+        map_error.append(float(re.split(r'[=,]',line)[1]))
+        #print(re.split(r'[=,]',line)[1])
+
+
+print("Min Loss: {}".format(min(loss)))
+plt.subplot(211)
 plt.plot(loss)
+plt.title('loss')
+plt.subplot(212)
+plt.plot(map_error)
+plt.title('map_error')
 plt.show()
