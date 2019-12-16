2865862f606015d2ce8232e48165a898acdbf3e3
Poisson <fc5ntb@splm.siemens.com>
2019-12-16 15:24:49 +0100

    add a log loss chart

--- scripts/tf_log_analyser.py
+++ scripts/tf_log_analyser.py
@@ -7,11 +7,13 @@ f = open('log.txt')
 loss_pattern = "INFO:tensorflow:loss = "
 map_pattern = "DetectionBoxes_Precision/mAP = "
 loss = []
+log_loss = []
 map_error = []
 
 for line in f:
     if re.search(loss_pattern, line):
         loss.append(float(re.split(r'[=,]',line)[1]))
+        log_loss.append(math.log(float(re.split(r'[=,]',line)[1])))
         #print(re.split(r'[=,]',line)[1])
     if re.search(map_pattern, line):
         map_error.append(float(re.split(r'[=,]',line)[1]))
@@ -19,10 +21,15 @@ for line in f:
 
 
 print("Min Loss: {}".format(min(loss)))
-plt.subplot(211)
+plt.subplot(311)
 plt.plot(loss)
 plt.title('loss')
-plt.subplot(212)
+
+plt.subplot(312)
+plt.plot(log_loss)
+plt.title('log loss')
+
+plt.subplot(313)
 plt.plot(map_error)
 plt.title('map_error')
 plt.show()
