d6c52cecc40cd0bce0f6714f6b6439ed6916ba2a
Alexandre <you@example.com>
2020-01-12 19:13:16 +0100

    add video inference in run_inference.py

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,25 +1,27 @@
-# Script that is running inference
+
 """
+Script that is running inference, based on a set of images or a video stream
 Example of usage:
   python run_inference.py -g D:\TensorFlow\private_project\convert_to_tflite\input\frozen_inference_graph.pb -i D:\TensorFlow\private_project\dooble_pics\inf_test -l D:\TensorFlow\private_project\annotations\label_map.pbtxt
 """
-import os
-import argparse
-path_to_model = r'D:\TensorFlow\models'
-# path_to_model = r'/Users/Alexandre/TensorFlow/models-master'
 
 """
+# Section below is to be reworked. It is currently disabled, the user is expected to have 
+path_to_model = r'D:\TensorFlow\models'
+# path_to_model = r'/Users/Alexandre/TensorFlow/models-master'
 var_env = ':' + path_to_model + '/research/:' + path_to_model + '/research/slim'
 print(var_env)
 os.environ['PYTHONPATH'] += var_env
+sys.path.append(os.path.join(path_to_model,'research'))
+sys.path.append(os.path.join(path_to_model,'utils'))
 """
 
-#Note : this script  needs the protos file to be created
 """
+#Note : this script  needs the protos file to be created
 (base) D:\TensorFlow\models\research>conda activate tensorflow_115
 (tensorflow_115) D:\TensorFlow\models\research>protoc object_detection/protos/*.proto --python_out=.
 """
-
+import argparse
 import numpy as np
 import os
 import sys
@@ -29,8 +31,7 @@ from matplotlib import pyplot as plt
 
 from PIL import Image
 import glob, os
-sys.path.append(os.path.join(path_to_model,'research'))
-sys.path.append(os.path.join(path_to_model,'utils'))
+
 from generate_labelimg_annotation_xml import create_label_file
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
@@ -38,6 +39,66 @@ from object_detection.utils import visualization_utils as vis_util
 matplotlib.use("TkAgg")
 # May meet problem when loading a new pb file
 tf.contrib.resampler
+
+def run_inference_video_stream(detection_graph, category_index):
+  import cv2
+  cap = cv2.VideoCapture(0)
+  while True:
+    ret, image_np = cap.read()
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    # Actual detection.
+    output_dict = run_inference_for_single_image(image_np, detection_graph)
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks'),
+      use_normalized_coordinates=True,
+      line_thickness=8)
+    cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
+
+def run_inference_images(PATH_TO_TEST_IMAGES_DIR, detection_graph, category_index):
+  # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+  
+  for image_path in glob.glob("{}/*.jpg".format(PATH_TO_TEST_IMAGES_DIR)):
+    print("Image path in run_inference.py: {}".format(image_path))
+    image = Image.open(image_path)
+    # the array based representation of the image will be used later in order to prepare the
+    # result image with boxes and labels on it.
+    image_np = load_image_into_numpy_array(image)
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    # Actual detection.
+    output_dict = run_inference_for_single_image(image_np, detection_graph)
+    # Visualization of the results of a detection.
+
+    # Create a labelImg xml file
+    create_label_file(output_dict, os.path.abspath(image_path))
+
+    # Display Inference result
+    plt.show(block=False)
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    plt.figure()
+    plt.imshow(image_np)
+  plt.show()
+
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
@@ -90,7 +151,6 @@ def run_inference_for_single_image(image, graph):
   return output_dict
 
 
-
 def main():
   print("Start")
   # Initiate argument parser
@@ -109,18 +169,18 @@ def main():
                       "--inputLabel",
                       help="Path to the label file: eg: annotations/label_map.pbtxt", 
                       type=str)
+  parser.add_argument("-s", "--inputSource", help="input source: video or image", type=str)
   args = parser.parse_args()
 
   if(args.inputGraph is None):
     raise AttributeError("inputGraph")
-  if(args.inputFolder is None):
+  if(args.inputFolder is None and args.inputSource is "image"):
     raise AttributeError("inputFolder")
   if(args.inputLabel is None):
     raise AttributeError("inputLabel")
+  if(args.inputSource is None):
+    args.inputSource="image"
 
-
-  # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-  PATH_TO_TEST_IMAGES_DIR = args.inputFolder
 
   # What model to download.
   # Path to frozen detection graph. This is the actual model that is used for the object detection.
@@ -142,38 +202,18 @@ def main():
   label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
   categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
   category_index = label_map_util.create_category_index(categories)
-  #os.chdir(PATH_TO_TEST_IMAGES_DIR)
   
-  for image_path in glob.glob("{}/*.jpg".format(PATH_TO_TEST_IMAGES_DIR)):
-    print("Image path in run_inference.py: {}".format(image_path))
-    image = Image.open(image_path)
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-    image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    # Visualization of the results of a detection.
+  input_source = args.inputSource
 
-    # Create a labelImg xml file
-    create_label_file(output_dict, os.path.abspath(image_path))
+  if input_source == "image":
+    PATH_TO_TEST_IMAGES_DIR = args.inputFolder
+    run_inference_images(PATH_TO_TEST_IMAGES_DIR, detection_graph, category_index)
+  elif input_source == "video":
+    run_inference_video_stream(detection_graph, category_index)
+  else:
+    print("bad argument")
 
-    # Display Inference result
-    plt.show(block=False)
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    plt.figure()
-    plt.imshow(image_np)
-  plt.show()
-
+  
 
 if __name__ == '__main__':
     main()
